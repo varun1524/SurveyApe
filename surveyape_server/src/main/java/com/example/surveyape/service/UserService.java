18fe8e361b67448316d519b2f86eb34bbccd5024
varun1524 <varun3273@gmail.com>
2018-04-20 22:56:51 -0700

    cleanup

--- surveyape_server/src/main/java/com/example/surveyape/service/UserService.java
+++ surveyape_server/src/main/java/com/example/surveyape/service/UserService.java
@@ -2,10 +2,7 @@ package com.example.surveyape.service;
 
 import com.example.surveyape.entity.User;
 import com.example.surveyape.repository.UserRepository;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,34 +10,25 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public ResponseEntity saveUser(User user){
-        ResponseEntity responseEntity = new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+    public User saveUser(User user){
+        User user1 = null;
         try{
-            User user1 = userRepository.save(user);
-            if(user1!=null){
-                responseEntity = new ResponseEntity(user1, HttpStatus.OK);
-            }
+            user1 = userRepository.save(user);
         }
         catch (Exception e){
-            e.printStackTrace();
+            throw e;
         }
-        return responseEntity;
+        return user;
     }
 
-    public ResponseEntity findByEmailAndPassword(String email, String password){
-        ResponseEntity responseEntity = new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+    public User findByEmailAndPassword(String email, String password){
+        User user = null;
         try{
-            User user = userRepository.findByEmailAndPassword(email, password);
-            if(user!=null){
-                JSONObject jsonObject = new JSONObject(user);
-                jsonObject.remove("password");
-                System.out.println(jsonObject);
-                responseEntity = new ResponseEntity(jsonObject.toString(), HttpStatus.OK);
-            }
+            user = userRepository.findByEmailAndPassword(email, password);
         }
         catch (Exception e){
-            e.printStackTrace();
+            throw e;
         }
-        return responseEntity;
+        return user;
     }
 }
