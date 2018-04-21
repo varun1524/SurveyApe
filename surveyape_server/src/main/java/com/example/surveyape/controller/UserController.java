2baca09cf87f8707a61478b76f23e5323d496ed3
varun1524 <varun3273@gmail.com>
2018-04-20 22:56:07 -0700

    bugfix:allowcredential added and validatesession implemented

--- surveyape_server/src/main/java/com/example/surveyape/controller/UserController.java
+++ surveyape_server/src/main/java/com/example/surveyape/controller/UserController.java
@@ -4,12 +4,14 @@ import com.example.surveyape.entity.User;
 import com.example.surveyape.service.UserService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@CrossOrigin(origins = "*")
+import javax.servlet.http.HttpSession;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RequestMapping(path = "/user")
 public class UserController {
 
@@ -18,14 +20,57 @@ public class UserController {
 
     @PostMapping(path = "/signUp")
     public ResponseEntity signup(@RequestBody String body){
-        JSONObject jsonObject = new JSONObject(body);
-        User user = new User(jsonObject.getString("email"), jsonObject.getString("firstname"), jsonObject.getString("lastname"), jsonObject.getString("password"));
-        return userService.saveUser(user);
+        ResponseEntity responseEntity = new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        try {
+            JSONObject jsonObject = new JSONObject(body);
+            User user = new User(jsonObject.getString("email"), jsonObject.getString("firstname"), jsonObject.getString("lastname"), jsonObject.getString("password"));
+            user = userService.saveUser(user);
+            if(user!=null){
+                responseEntity = new ResponseEntity(user, HttpStatus.OK);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return responseEntity;
     }
 
     @PostMapping(path = "/login")
-    public ResponseEntity login(@RequestBody String body){
-        JSONObject jsonObject = new JSONObject(body);
-        return userService.findByEmailAndPassword(jsonObject.getString("email"), jsonObject.getString("password"));
+    public ResponseEntity login(@RequestBody String body, HttpSession httpSession){
+        ResponseEntity responseEntity = new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        try{
+            JSONObject jsonObject = new JSONObject(body);
+            User user = userService.findByEmailAndPassword(jsonObject.getString("email"), jsonObject.getString("password"));
+            if(user!=null){
+                httpSession.setAttribute("email", jsonObject.getString("email"));
+                jsonObject = new JSONObject(user);
+                jsonObject.remove("password");
+                System.out.println(jsonObject);
+                responseEntity = new ResponseEntity(jsonObject.toString(), HttpStatus.OK);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return responseEntity;
+    }
+
+    @PostMapping(path = "/validateSession")
+    public ResponseEntity validateSession(HttpSession session){
+        ResponseEntity responseEntity = null;
+        try{
+            System.out.println(session.getAttribute("email"));
+            if (session.getAttribute("email") == null) {
+                responseEntity = new ResponseEntity(null, HttpStatus.NOT_FOUND);
+            } else {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.append("email", session.getAttribute("email"));
+                responseEntity = new ResponseEntity(jsonObject.toString(), HttpStatus.OK);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return responseEntity;
     }
 }
