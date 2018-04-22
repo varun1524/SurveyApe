abb355d31c641e17ef26fcb0c303c424da01c2d5
RutvikatHub <pensionwar.rutvik@gmail.com>
2018-04-21 18:41:44 -0700

    update

--- surveyape_client/src/components/DemoSignUp.js
+++ surveyape_client/src/components/DemoSignUp.js
@@ -9,32 +9,28 @@ class DemoSignUp extends Component {
     constructor() {
         super();
         this.state = {
-
             firstname: "",
             lastname: "",
             email: "",
             password: "",
-            isLoggedIn:false,
-            message:""
-
-
-
+            isLoggedIn: false,
+            message: ""
         };
     }
 
-    handleSignUp(){
-        console.log("[Signup Component] handleSignUp() userdetail: "+this.state);
+    handleSignUp() {
+        console.log("[Signup Component] handleSignUp() userdetail: " + this.state);
 
         API.doSignUp(this.state)
             .then((response) => {
-
                 if (response.status === 200) {
                     this.setState({
                         ...this.state,
                         isLoggedIn: true,
                         message: "You have successfully signed up. Please login here"
                     });
-                    this.handlePageChange("/")
+                    this.props.history.push("/");
+
                 } else if (response.status === 401) {
                     console.log("State");
                     this.setState({
@@ -44,7 +40,7 @@ class DemoSignUp extends Component {
                     });
                     // this.props.history.push("/signup")
                 }
-                else if(response.status === 301){
+                else if (response.status === 301) {
                     this.setState({
                         ...this.state,
                         isLoggedIn: false,
@@ -52,13 +48,13 @@ class DemoSignUp extends Component {
                     });
                     // this.props.history.push("/signup")
                 }
-                else
-                {
+                else {
                     this.setState({
                         ...this.state,
                         isLoggedIn: false,
                         message: "Error while signing up."
                     });
+
                 }
             });
     }
@@ -66,36 +62,43 @@ class DemoSignUp extends Component {
     render() {
         return (
             <div className="DemoSignUp">
-                <HeaderComponent />
+                <HeaderComponent/>
                 <div className="sign-up-form">
                     <form>
                         <div className="sign-up-container">
                             <input type="text" placeholder="Enter Username" name="email"
-                                   onChange={(event) => {this.setState({
+                                   onChange={(event) => {
+                                       this.setState({
                                            ...this.state,
                                            email: event.target.value
                                        })
                                    }}/>
                             <input type="password" placeholder="Enter Password" name="password"
-                                   onChange={(event) => {this.setState({
-                                       ...this.state,
-                                       password: event.target.value
-                                        })
+                                   onChange={(event) => {
+                                       this.setState({
+                                           ...this.state,
+                                           password: event.target.value
+                                       })
                                    }}/>
                             <input type="text" placeholder="Enter First Name" name="firstname"
-                                   onChange={(event) => {this.setState({
-                                       ...this.state,
-                                       firstname: event.target.value
-                                        })
+                                   onChange={(event) => {
+                                       this.setState({
+                                           ...this.state,
+                                           firstname: event.target.value
+                                       })
                                    }}/>
                             <input type="text" placeholder="Enter Second Name" name="lastname"
-                                   onChange={(event) => {this.setState({
-                                       ...this.state,
-                                       lastname: event.target.value
-                                        })
+                                   onChange={(event) => {
+                                       this.setState({
+                                           ...this.state,
+                                           lastname: event.target.value
+                                       })
                                    }}/>
 
-                            <button type="submit" onClick={()=>this.handleSignUp()}>Sign Up</button>
+                            {/*<button onClick={() => this.handleSignUp()}>Sign Up</button>*/}
+
+                            <input type="button" id="button" className="btn btn-primary col-sm-8 col-md-8 col-lg-8"
+                                   onClick={()=>this.handleSignUp()} value="Sign Up"/>
 
                             <span className="sign-up-password">Have an account ? <a href="/">Login</a></span>
                         </div>
