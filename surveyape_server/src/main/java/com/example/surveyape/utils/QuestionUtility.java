b695288abab2fe293d9573578870ccc3c29e16a8
varun1524 <varun3273@gmail.com>
2018-04-28 04:57:06 -0700

    Question Types

---
+++ surveyape_server/src/main/java/com/example/surveyape/utils/QuestionUtility.java
@@ -0,0 +1,10 @@
+package com.example.surveyape.utils;
+
+public class QuestionUtility {
+    public static final String CHECKBOX = "checkbox";
+    public static final String RADIO = "radio";
+    public static final String DROPDOWN = "dropdown";
+    public static final String YESNO = "yesno";
+    public static final String SHORTANSWER = "shortanswer";
+    public static final String RATING = "rating";
+}
