067edf5974bbaa170b5f98c2c6294a04f3582a3d
RutvikatHub <pensionwar.rutvik@gmail.com>
2018-04-27 20:39:44 -0700

    survey builder component boilerplate

--- surveyape_client/src/components/createsurvey/questiondashboard.js
+++ surveyape_client/src/components/createsurvey/questiondashboard.js
@@ -5,41 +5,65 @@ import QuestionComponent from './questioncomponent';
 class QuestionDashboard extends Component {
 
     constructor() {
+
         super();
+        console.log("constructor questiondashboard")
         this.state = {
 
         }
     }
 
-    displayQuestionComponent() {
-        console.log("In displayQuestionComponent");
-        console.log(this.props.state.questionReducer);
 
-        return this.props.state.questionReducer.map((question) => {
+    displayQuestionComponent() {
+        // console.log("In displayQuestionComponent");
+        // console.log(this.props.state.questionReducer);
+
+        return this.props.survey.questions.map((question, index) => {
             return(
-                <QuestionComponent question_type = {question.question_type}/>
+                <QuestionComponent question_type = {question.question_type} index_id ={index}/>
             )
         });
     }
 
-    render () {
-        if(!this.props.state.questionReducer) {
-            console.log("questionReducer null");
-            return (
-                <div>questionReducer null</div>
-            )
+    getPublishandSave(){
+        if(this.props.survey.questions.length>0){
+            return(<div>
+                <div>
+                    <button type="button">Save</button>
+                </div>
+                <div>
+                    <button type="button">Publish</button>
+                </div>
+            </div>);
         }
+
+
+
+    }
+
+
+
+    render () {
+        console.log("render questiondashboard")
+        // if(!this.props.state.questionReducer) {
+        //     console.log("questionReducer null");
+        //     return (
+        //         <div>Empty div</div>
+        //     )
+        // }
         return(
             <div className="QuestionDashboard">
                 {this.displayQuestionComponent()}
+                {this.getPublishandSave()}
             </div>
         );
     }
 }
 
 function mapStateToProps(state) {
+    console.log("mapstoprops questiondashboard",state.survey)
     return{
-        state: state
+        survey: state.survey
     }
 }
 
---
+++ surveyape_client/src/reducers/survey.js
@@ -0,0 +1,66 @@
+import {actionTypes} from "../actions/actionTypes";
+
+const survey_data = {
+    survey_name: "",
+    survey_type:"",
+    questions:[]
+}
+
+const survey = (state = survey_data, action)=>
+{
+    switch (action.type) {
+        case actionTypes.CREATE_SURVEY :
+            console.log("CREATE_SURVEY reducer");
+
+            return Object.assign({},state,{
+                survey_name:action.data.survey_name,
+                survey_type:action.data.survey_type
+
+            });
+        case actionTypes.ADD_QUESTION :
+            console.log("ADD_QUESTION reducer",action.question_type);
+            let add_questions = state.questions;
+            add_questions.push({
+                question_text:"",
+                question_type:action.question_type,
+                options:[]
+            });
+            return Object.assign({},state,{
+                questions:add_questions
+            });
+        case actionTypes.EDIT_QUESTION :
+            // action data will pass question index and text to be edited
+            console.log("EDIT_QUESTION reducer",action.data);
+            let edit_questions = state.questions;
+            edit_questions[action.data.question_index].question_text = action.data.question_text;
+            return Object.assign({},state, {
+                questions: edit_questions
+            });
+
+        case actionTypes.ADD_OPTIONS :
+            console.log("ADD_OPTIONS reducer",action.data.question_index);
+            // action data will pass question index and option text value
+            let edit_questions1 = state.questions;
+            edit_questions1[action.data.question_index].options.push({
+                option_type:"text",
+                option_text:""
+            });
+
+            return Object.assign({},state,{
+                questions:edit_questions1
+            });
+
+        case actionTypes.EDIT_OPTIONS :
+            // action data will pass question index, option index and option text value
+            console.log("EDIT_QUESTION reducer",action.data);
+            let edit_questions2 = state.questions;
+            edit_questions2[action.data.question_index].options[action.data.option_index].option_text =action.data.option_text;
+            return Object.assign({},state,{
+                questions:edit_questions2
+            });
+        default :
+            return state;
+    }
+};
+
+export default survey;
