3a5d6b24e49fd94c6c9978769b4fdc89b2b8e6df
ARIJIT MANDAL <oori@ARIJITs-MacBook-Pro.local>
2018-04-29 01:10:19 -0700

    front end api to get survey by id

--- surveyape_client/src/api/API.js
+++ surveyape_client/src/api/API.js
@@ -153,3 +153,19 @@ export const deleteOption = (optionId, surveyId) =>
         console.log(error);
         return error;
     });
+
+    export const getSurveyById = (surveyId) =>
+        fetch(`${api}/survey/${surveyId}`, {
+            method: 'GET',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            credentials:'include'
+        }).then(res => {
+            return res;
+        }).catch(error => {
+            console.log("This is error");
+            console.log(error);
+            return error;
+        });
