08b500032c7687fbe67412a2d6fb76f4804b0151
RutvikatHub <pensionwar.rutvik@gmail.com>
2018-05-03 02:06:41 -0700

    added verification modal. success/fail cases handled

--- surveyape_client/src/components/login.js
+++ surveyape_client/src/components/login.js
@@ -78,6 +78,7 @@ class Login extends Component {
     });
 
     render() {
+        console.log("[signin] render method");
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
         return (
             <div className="DemoSignIn">
