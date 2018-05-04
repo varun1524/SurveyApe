e8b9a7bb5bfc00ae42b986c20fa48c35bc474ac1
varun1524 <varun3273@gmail.com>
2018-05-04 12:48:52 -0700

    url route for Open and Close survey added

--- surveyape_client/src/App.js
+++ surveyape_client/src/App.js
@@ -87,6 +87,18 @@ class App extends Component {
                             {...match}
                         />)}
                     />
+                    <Route path= "/surveyresponse/c/:cresponse_id" render = {(match) => (
+                        <SurveyResponse
+                            handlePageChange = {this.handlePageChange}
+                            {...match}
+                        />)}
+                    />
+                    <Route path= "/surveyresponse/o/:oresponse_id" render = {(match) => (
+                        <SurveyResponse
+                            handlePageChange = {this.handlePageChange}
+                            {...match}
+                        />)}
+                    />
                 </Switch>
             </div>
         );
